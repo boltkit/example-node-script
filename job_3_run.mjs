643e770d2b398d24be5e0b55f0473ff50ef60799
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2024-08-11 12:56:30 +0200

    naming

--- job_3_run.mjs
+++ job_3_run.mjs
@@ -1,9 +1,9 @@
 import {getJobResultJson} from "./node-bolt-helper.mjs"
 
-const jobres0 = getJobResultJson(0)
-const jobres1 = getJobResultJson(1)
-const jobres2 = getJobResultJson(2)
+const color = getJobResultJson(0)
+const temp = getJobResultJson(1)
+const time = getJobResultJson(2)
 
-console.log(jobres0)
-console.log(jobres1)
-console.log(jobres2)
+console.log(color)
+console.log(temp)
+console.log(time)
